d8b6aced1723a1ceb774eec783a1ea6e2c28de9c
supfiger <danilzin777@gmail.com>
2020-06-09 10:25:59 +0300

    Updates

---
+++ precache-manifest.36274fad03ba3790ef505be41805d80f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9110ef456ac9e95c6a912439d3820d",
+    "url": "/mobx-react-todo/index.html"
+  },
+  {
+    "revision": "84226f34911883054de2",
+    "url": "/mobx-react-todo/static/css/main.01ae0798.chunk.css"
+  },
+  {
+    "revision": "1afa64b5a9cf15e57ec8",
+    "url": "/mobx-react-todo/static/js/2.9d1c594a.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/mobx-react-todo/static/js/2.9d1c594a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84226f34911883054de2",
+    "url": "/mobx-react-todo/static/js/main.102cf945.chunk.js"
+  },
+  {
+    "revision": "da4916acf81be973e506",
+    "url": "/mobx-react-todo/static/js/runtime-main.d3ac492d.js"
+  }
+]);
